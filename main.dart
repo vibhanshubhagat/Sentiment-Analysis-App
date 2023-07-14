dca63bb869114bd83f310f2b5a5fa5d2b94b4701
vibhanshubhagat <122006611+vibhanshubhagat@users.noreply.github.com>
2023-07-15 00:40:40 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -47,7 +47,7 @@ class _EmotionAnalyzerHomePageState extends State<EmotionAnalyzerHomePage> {
       });
 
       try {
-        var response = await dio.post("http://10.0.2.2:5003/post_json",
+        var response = await dio.post("http://192.168.0.72:5003/post_json",
             data: {"byte_string": img64});
 
         setState(() {
@@ -82,7 +82,7 @@ class _EmotionAnalyzerHomePageState extends State<EmotionAnalyzerHomePage> {
       });
 
       try {
-        var response = await dio.post("http://10.0.2.2:5003/post_json",
+        var response = await dio.post("http://192.168.0.72:5003/post_json",
             data: {"byte_string": img64});
 
         setState(() {
@@ -127,111 +127,123 @@ class _EmotionAnalyzerHomePageState extends State<EmotionAnalyzerHomePage> {
       backgroundColor:
           Color.fromRGBO(221, 205, 197, 1), // Set the background color
       body: Stack(children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'HOW ARE YOU FEELING TODAY?',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              'TELL US WITH A SELFIE :)',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 20),
-            Container(
-              width: 350,
-              height: 350,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-              ),
-              child: displayimage != null
-                  ? Image.file(displayimage!)
-                  : Container(),
-            ),
-            SizedBox(height: 40),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: capturePhoto,
-                  style: ElevatedButton.styleFrom(
-                    primary:
-                        Color.fromRGBO(164, 112, 90, 1), // Set the button color
-                  ),
-                  child: Text('Take Photo'),
-                ),
-                SizedBox(width: 40),
-                ElevatedButton(
-                  onPressed: uploadPhoto,
-                  style: ElevatedButton.styleFrom(
-                    primary:
-                        Color.fromRGBO(164, 112, 90, 1), // Set the button color
-                  ),
-                  child: Text('Upload Photo'),
-                ),
-              ],
-            ),
-            SizedBox(height: 5),
-            const SizedBox(
-                height:
-                    40), // Increase the space between the previous text and this text
-            Text(
-              emotionOutput.isNotEmpty
-                  ? 'Your Emotion(s): $emotionOutput!'
-                  : "",
-              style: TextStyle(
-                fontSize: 26,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-                height:
-                    20), // Increase the space between the previous text and this text
-            Row(
+        SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10),
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  'Incorrect?Move closer to camera',
+                  'HOW ARE YOU FEELING TODAY?',
                   style: TextStyle(
-                    color: Colors.redAccent,
-                    fontSize: 15,
+                    fontSize: 20,
                     fontWeight: FontWeight.bold,
                   ),
+                  textAlign: TextAlign.center,
                 ),
-                SizedBox(width: 8),
-                ElevatedButton(
-                  onPressed: capturePhoto,
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.redAccent,
+                Text(
+                  'TELL US WITH A SELFIE :)',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
                   ),
-                  child: Text('Retake Photo'),
+                  textAlign: TextAlign.center,
                 ),
+                SizedBox(height: 20),
+                Container(
+                  width: 350,
+                  height: 350,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(16)),
+                    border: Border.all(color: Colors.black),
+                  ),
+                  child: displayimage != null
+                      ? Image.file(displayimage!)
+                      : Container(),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: capturePhoto,
+                      style: ElevatedButton.styleFrom(
+                        primary: Color.fromRGBO(
+                            164, 112, 90, 1), // Set the button color
+                      ),
+                      child: Text('Take Photo'),
+                    ),
+                    SizedBox(width: 40),
+                    ElevatedButton(
+                      onPressed: uploadPhoto,
+                      style: ElevatedButton.styleFrom(
+                        primary: Color.fromRGBO(
+                            164, 112, 90, 1), // Set the button color
+                      ),
+                      child: Text('Upload Photo'),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5),
+                const SizedBox(
+                    height:
+                        20), // Increase the space between the previous text and this text
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text(
+                    emotionOutput.isNotEmpty
+                        ? 'Your Emotion(s): $emotionOutput!'
+                        : "",
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(
+                    height:
+                        20), // Increase the space between the previous text and this text
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Incorrect? Move closer to camera',
+                      style: TextStyle(
+                        color: Colors.redAccent,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    ElevatedButton(
+                      onPressed: capturePhoto,
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.redAccent,
+                      ),
+                      child: Text('Retake Photo'),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                    height:
+                        20), // Increase the space between the previous text and this text
+                Center(
+                  // Center align the following text
+                  child: Text(
+                    'Connect with our Chatbot/Therapist Now!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black, // Set the color explicitly
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10)
               ],
             ),
-            SizedBox(
-                height:
-                    20), // Increase the space between the previous text and this text
-            Center(
-              // Center align the following text
-              child: Text(
-                'Connect with our Chatbot/Therapist Now!',
-                style: TextStyle(
-                  color: Colors.black, // Set the color explicitly
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
         Center(
           child: Visibility(
